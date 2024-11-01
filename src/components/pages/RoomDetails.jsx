9dbb049e4ede7ed5ac20bc6448104595ba2d2c00
DopeGrammerZA <Kagisomasebe98@gmail.com>
2024-11-01 05:27:48 +0200

    Styled the pages that are in my booking system

--- src/components/pages/RoomDetails.jsx
+++ src/components/pages/RoomDetails.jsx
@@ -16,32 +16,26 @@ const RoomDetail = () => {
   console.log(selectedRoom.images && selectedRoom.images[0]);
 
   return (
-    <div>
+    <div >
       <Navbar />
-      <div className="room-detail">
-        <h2 className="room-title">{selectedRoom.name}</h2>
-        <div className="room-images">
-          {selectedRoom.images && selectedRoom.images.length > 0 ? (
-            <>
-              <img className="large-image" src={selectedRoom.images[0]} alt={`Room ${selectedRoom.name} - Main`} />
-              <div className="small-images">
-                {selectedRoom.images.slice(1).map((image, index) => (
-                  <img key={index} src={image} alt={`Room ${selectedRoom.name} - Image ${index + 2}`} />
-                ))}
-              </div>
-            </>
-          ) : (
-            <p>No images available for this room.</p>
-          )}
-        </div>
-        <p><strong>Description:</strong> {selectedRoom.description}</p>
-        <p><strong>Location:</strong> {selectedRoom.location}</p>
-        <p><strong>Rating:</strong> {selectedRoom.rating} ⭐</p>
-        <p><strong>Price per night:</strong> R{selectedRoom.pricePerNight}</p>
-        <p><strong>Amenities:</strong> {Array.isArray(selectedRoom.amenities) ? selectedRoom.amenities.join(', ') : selectedRoom.amenities || 'No amenities listed'}</p>
+          <div className='room-detail-container'>
+          <div className="room-detail">
+            <h2 className="room-title">{selectedRoom.name}</h2>
+            <div className="room-images">
+            <img className="large-image" src={selectedRoom.roomImage} alt={`Room ${selectedRoom.name} - Main`} />
+            </div >
+            <div className='room-paragraphs'>
+                <p><strong>Description:</strong> {selectedRoom.description}</p>
+                <p><strong>Location:</strong> {selectedRoom.location}</p>
+                <p><strong>Rating:</strong> {selectedRoom.rating} ⭐</p>
+                <p><strong>Price per night:</strong> R{selectedRoom.pricePerNight}</p>
+                <p><strong>Amenities:</strong> {Array.isArray(selectedRoom.amenities) ? selectedRoom.amenities.join(', ') : selectedRoom.amenities || 'No amenities listed'}</p>
 
-        <button className="book-room-button" onClick={() => navigate('/confirm-room')}>Book Room</button>
+            </div>
+            <button className="book-room-button" onClick={() => navigate('/confirm-room')}>Book Room</button>
+          </div>
       </div>
+      
       <Footer />
     </div>
   );
